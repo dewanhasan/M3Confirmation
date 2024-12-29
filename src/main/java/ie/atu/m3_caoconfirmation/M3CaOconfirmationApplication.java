c6be757e032cc73459e0b9a2e14a996e3782d635
dewanhasan99 <g00410156@atu.ie>
2024-12-29 23:05:07 +0000

    First Commit

---
+++ src/main/java/ie/atu/m3_caoconfirmation/M3CaOconfirmationApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.m3_caoconfirmation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3CaOconfirmationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M3CaOconfirmationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/m3_caoconfirmation/M3CaOconfirmationApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.m3_caoconfirmation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M3CaOconfirmationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
